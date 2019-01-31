6edd6b53ab76ee7f06aaba71ce2d65c09c2d44ed
Sergei <artix.work@gmail.com>
2019-01-31 18:56:38 +0200

    +

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vueBem from '@verstaerker/vue-bem';
 
-Vue.use(vueBem, {
-  namespace: '',
-  blockSource: 'name',
-  method: '$bem',
-  hyphenate: {
-    blockAndElement: false,
-    modifier: true,
-  },
-  delimiters: {
-    element: '__',
-    modifier: '_',
-    value: '-',
-  },
-})
 Vue.config.productionTip = false
 
 new Vue({
